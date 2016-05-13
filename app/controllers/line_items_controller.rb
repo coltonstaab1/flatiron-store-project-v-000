c9729e77e9ed2ec989a335cddca718bc5a512c1f
coltonstaab1 <colton.staab@gmail.com>
2016-05-12 21:25:31 -0400

    Fixed the header and now passing 7 tests

--- app/controllers/line_items_controller.rb
+++ app/controllers/line_items_controller.rb
@@ -1,3 +1,13 @@
 class LineItemsController < ApplicationController
 
+  def create
+    if current_user.current_cart.nil?
+      cart = Cart.create
+      current_user.current_cart = cart
+      current_user.save
+    end
+    LineItem.create(:cart_id => current_user.current_cart.id, :item_id => params[:item_id])
+    redirect_to cart_path(current_user.current_cart)
+  end
+
 end
--- app/models/cart.rb
+++ app/models/cart.rb
@@ -11,19 +11,22 @@ class Cart < ActiveRecord::Base
   def checkout
     self.status = "submitted"
     self.save
+
+    change_inventory
+
     self.user.current_cart = nil
     self.user.save
-    change_inventory
+    
   end
 
   private
-    def change_inventory
-      if self.status == "submitted"
-        self.line_items.each do |line_item|
-          line_item.item.inventory -= line_item.quantity
-          line_item.item.save
-        end
+  def change_inventory
+    if self.status = "submitted"
+      self.line_items.each do |line_item| 
+        line_item.item.inventory -= line_item.quantity
+        line_item.item.save
       end
-    end
+    end 
+  end
 
 end
